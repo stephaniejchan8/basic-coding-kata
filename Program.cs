3d69381395405ee5ab78f8fb14c92ee7de34b988
Stephanie MYOB <93897509+stephaniejchan8@users.noreply.github.com>
2022-09-14 16:21:31 +1000

    refactoring

--- Program.cs
+++ Program.cs
@@ -1,58 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-// 1.
+﻿// Add data validation
 
-Console.WriteLine("Hello, World!");
+// 1. Hello, World!
+
+Console.WriteLine("Hello, World! \n");
+
 
 // 2., 3. asks the user for their name and only greets you if you are Alice or Bob.
-string name = "Alice";
+
+Console.WriteLine("Enter your name: ");
+var name = Console.ReadLine();
+name = name.Trim();
+
 if (name == "Alice" || name == "Bob")
 {
-    Console.WriteLine($"Hello {name}");
+    Console.WriteLine($"Hello {name} \n");
 }
+
 
 // 4. asks the user for a number n and prints the sum of the numbers 1 to n
-int number = 2;
-int sum = 0;
-for (int i = 1; i <= number; i++)
-{
-    sum += i;
+
+Console.WriteLine("Enter a number:");
+var numberString = Console.ReadLine();
+var number = Int32.Parse(numberString);
+
+var sum = SumToN(number);
+Console.WriteLine($"Sum of numbers from 1 to {number}: {sum}");
+
+int SumToN(int number)
+{ 
+    var sum = 0;
+    for (var i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 }
 
-Console.WriteLine($"Sum of numbers from 1 to {number}: {sum}");
 
 // 5. asks the user for a number n and prints the sum of the numbers 1 to n if the number is a multiple of three or five
 
-int numberMultiple = 17;
-int sumMultiple = 0;
+Console.WriteLine("Enter a number:");
+var numberMultipleString = Console.ReadLine();
+var numberMultiple = Int32.Parse(numberMultipleString);
+
+var sumMultipleOf3or5 = 0;
 
 for (int i = 0; i <= numberMultiple; i++)
 {
     if (i % 3 == 0 || i % 5 == 0)
     {
-        sumMultiple += i;
+        sumMultipleOf3or5 += i;
     }
 }
 
-Console.WriteLine($"Sum of numbers from 1 to {numberMultiple} that are a multiple of 3 or 5: {sumMultiple}");
+Console.WriteLine($"Sum of numbers from 1 to {numberMultiple} that are a multiple of 3 or 5: {sumMultipleOf3or5}");
+
 
 // 6. asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n
-int numberOption = 4;
-string sumOrProduct = "product";
-string formatSumOrProduct = char.ToUpper(sumOrProduct[0]) + sumOrProduct.Substring(1).ToLower();
-Console.WriteLine(formatSumOrProduct);
-int optionTotal = 0;
-//string optionConsole = $"{formatSumOrProduct} of numbers from 1 to {numberOption}: {optionTotal}";
+Console.WriteLine("Enter a number:");
+var numberOptionString = Console.ReadLine();
+var numberOption = Int32.Parse(numberOptionString);
 
-if (formatSumOrProduct == "Sum")
+Console.WriteLine("Would you like to calculate the sum or product of the numbers from 1 to your input number?");
+var sumOrProduct = Console.ReadLine();
+sumOrProduct = sumOrProduct.ToLower();
+
+var optionTotal = 0;
+var optionConsole = $"{sumOrProduct} of numbers from 1 to {numberOption}: ";
+
+
+if (numberOption == 0 && (sumOrProduct == "sum" || sumOrProduct == "product"))
 {
-    for (int i = 1; i <= numberOption; i++)
-    {
-        optionTotal += i;
-    }
+    Console.WriteLine(optionConsole + optionTotal);
+} 
 
-    Console.WriteLine($"{formatSumOrProduct} of numbers from 1 to {numberOption}: {optionTotal}");
+else if (sumOrProduct == "sum")
+{
+    optionTotal = SumToN(numberOption);
+    Console.WriteLine(optionConsole + optionTotal);
 }
-else if (formatSumOrProduct == "Product")
+
+else if (sumOrProduct == "product")
 {
     optionTotal = 1;
     for (int i = 1; i <= numberOption; i++)
@@ -60,10 +89,23 @@ else if (formatSumOrProduct == "Product")
         optionTotal *= i;
     }
 
-    Console.WriteLine($"{formatSumOrProduct} of numbers from 1 to {numberOption}: {optionTotal}");
+    Console.WriteLine(optionConsole + optionTotal);
 }
 
 // 7. Write a program that prints a multiplication table for numbers up to 12.
+
+Console.WriteLine("Multiplication table: enter a number from 1 to 12 (inclusive):");
+var multiplicationNumberString = Console.ReadLine();
+var multiplicationNumber = Int32.Parse(multiplicationNumberString);
+
+if (1 <= multiplicationNumber && multiplicationNumber <= 12)
+{
+    for (var i = 1; i <= multiplicationNumber; i++)
+    {
+        var multiple = i * multiplicationNumber;
+        Console.WriteLine($"{i} x {multiplicationNumber} = {multiple}");
+    }
+}
 
 // 8. Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
 
@@ -79,6 +121,7 @@ else if (formatSumOrProduct == "Product")
 
 int currentYr = DateTime.Today.Year;
 int counterLeap = 0;
+
 for (int i = currentYr; counterLeap < 20; i++)
 {
     if (i % 4 == 0)
