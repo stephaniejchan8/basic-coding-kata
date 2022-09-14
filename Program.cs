08324ae2be07b059a689d4dbdd17e4ba4f415d79
Stephanie MYOB <93897509+stephaniejchan8@users.noreply.github.com>
2022-09-14 16:46:01 +1000

    added try catch for int parse

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// Add data validation
 
+var invalidDataMessage = "Input data is not in correct format. \n";
+
 // 1. Hello, World!
 
 Console.WriteLine("Hello, World! \n");
@@ -15,19 +17,32 @@ if (name == "Alice" || name == "Bob")
 {
     Console.WriteLine($"Hello {name} \n");
 }
+else
+{
+    Console.WriteLine("Incorrect name entered. \n");
+}
 
 
 // 4. asks the user for a number n and prints the sum of the numbers 1 to n
 
 Console.WriteLine("Enter a number:");
 var numberString = Console.ReadLine();
-var number = Int32.Parse(numberString);
 
-var sum = SumToN(number);
-Console.WriteLine($"Sum of numbers from 1 to {number}: {sum}");
+try
+{
+    var number = Int32.Parse(numberString);
+
+    var sum = SumToN(number);
+    Console.WriteLine($"Sum of numbers from 1 to {number}: {sum} \n");
+}
+catch (FormatException)
+{
+    Console.WriteLine(invalidDataMessage);
+}
+
 
 int SumToN(int number)
-{ 
+{
     var sum = 0;
     for (var i = 1; i <= number; i++)
     {
@@ -42,69 +57,96 @@ int SumToN(int number)
 
 Console.WriteLine("Enter a number:");
 var numberMultipleString = Console.ReadLine();
-var numberMultiple = Int32.Parse(numberMultipleString);
 
-var sumMultipleOf3or5 = 0;
-
-for (int i = 0; i <= numberMultiple; i++)
+try
 {
-    if (i % 3 == 0 || i % 5 == 0)
-    {
-        sumMultipleOf3or5 += i;
-    }
-}
+    var numberMultiple = Int32.Parse(numberMultipleString);
 
-Console.WriteLine($"Sum of numbers from 1 to {numberMultiple} that are a multiple of 3 or 5: {sumMultipleOf3or5}");
+    var sumMultipleOf3or5 = 0;
+
+    for (int i = 0; i <= numberMultiple; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sumMultipleOf3or5 += i;
+        }
+    }
+
+    Console.WriteLine(
+        $"Sum of numbers from 1 to {numberMultiple} that are a multiple of 3 or 5: {sumMultipleOf3or5} \n");
+}
+catch (FormatException)
+{
+    Console.WriteLine(invalidDataMessage);
+}
 
 
 // 6. asks the user for a number n and gives them the possibility to choose between computing the sum and computing the product of 1,…,n
 Console.WriteLine("Enter a number:");
 var numberOptionString = Console.ReadLine();
-var numberOption = Int32.Parse(numberOptionString);
 
-Console.WriteLine("Would you like to calculate the sum or product of the numbers from 1 to your input number?");
-var sumOrProduct = Console.ReadLine();
-sumOrProduct = sumOrProduct.ToLower();
-
-var optionTotal = 0;
-var optionConsole = $"{sumOrProduct} of numbers from 1 to {numberOption}: ";
-
-
-if (numberOption == 0 && (sumOrProduct == "sum" || sumOrProduct == "product"))
+try
 {
-    Console.WriteLine(optionConsole + optionTotal);
-} 
+    var numberOption = Int32.Parse(numberOptionString);
 
-else if (sumOrProduct == "sum")
-{
-    optionTotal = SumToN(numberOption);
-    Console.WriteLine(optionConsole + optionTotal);
-}
+    Console.WriteLine("Would you like to calculate the sum or product of the numbers from 1 to your input number?");
+    var sumOrProduct = Console.ReadLine();
+    sumOrProduct = sumOrProduct.ToLower();
 
-else if (sumOrProduct == "product")
-{
-    optionTotal = 1;
-    for (int i = 1; i <= numberOption; i++)
+    var optionTotal = 0;
+    var optionConsole = $"{sumOrProduct} of numbers from 1 to {numberOption}: ";
+
+
+    if (numberOption == 0 && (sumOrProduct == "sum" || sumOrProduct == "product"))
     {
-        optionTotal *= i;
+        Console.WriteLine(optionConsole + optionTotal);
     }
 
-    Console.WriteLine(optionConsole + optionTotal);
+    else if (sumOrProduct == "sum")
+    {
+        optionTotal = SumToN(numberOption);
+        Console.WriteLine(optionConsole + optionTotal);
+    }
+
+    else if (sumOrProduct == "product")
+    {
+        optionTotal = 1;
+        for (int i = 1; i <= numberOption; i++)
+        {
+            optionTotal *= i;
+        }
+
+        Console.WriteLine(optionConsole + optionTotal);
+    }
+
+    Console.WriteLine("\n");
+}
+catch (FormatException)
+{
+    Console.WriteLine(invalidDataMessage);
 }
 
 // 7. Write a program that prints a multiplication table for numbers up to 12.
 
 Console.WriteLine("Multiplication table: enter a number from 1 to 12 (inclusive):");
 var multiplicationNumberString = Console.ReadLine();
-var multiplicationNumber = Int32.Parse(multiplicationNumberString);
 
-if (1 <= multiplicationNumber && multiplicationNumber <= 12)
+try
 {
-    for (var i = 1; i <= multiplicationNumber; i++)
+    var multiplicationNumber = Int32.Parse(multiplicationNumberString);
+
+    if (1 <= multiplicationNumber && multiplicationNumber <= 12)
     {
-        var multiple = i * multiplicationNumber;
-        Console.WriteLine($"{i} x {multiplicationNumber} = {multiple}");
+        for (var i = 1; i <= multiplicationNumber; i++)
+        {
+            var multiple = i * multiplicationNumber;
+            Console.WriteLine($"{i} x {multiplicationNumber} = {multiple} \n");
+        }
     }
+}
+catch
+{
+    Console.WriteLine(invalidDataMessage);
 }
 
 // 8. Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
@@ -122,11 +164,12 @@ if (1 <= multiplicationNumber && multiplicationNumber <= 12)
 int currentYr = DateTime.Today.Year;
 int counterLeap = 0;
 
+Console.WriteLine("The next 20 leap years:");
 for (int i = currentYr; counterLeap < 20; i++)
 {
     if (i % 4 == 0)
     {
-        Console.WriteLine(i);
+        Console.Write($"{i} ");
         counterLeap++;
     }
 }
