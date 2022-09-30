b88a2869204ef93085620c48443c2b24f1db10ae
Stephanie MYOB <93897509+stephaniejchan8@users.noreply.github.com>
2022-09-30 11:30:51 +1000

    refactored guessing game

--- Guessing/GuessingGame.cs
+++ Guessing/GuessingGame.cs
@@ -3,7 +3,6 @@ namespace f_basic_coding.Guessing;
 public class GuessingGame
 {
     // 8. Write a guessing game where the user has to guess a secret number. After every guess the program tells the user whether their number was too large or too small. At the end the number of tries needed should be printed. It counts only as one try if they input the same number multiple times consecutively.
-    // private int _secretNumber;
     private UserInput _userInput = new UserInput();
     public int GuessCounter = 0;
 
@@ -12,15 +11,12 @@ public class GuessingGame
         var secretNumber = ObtainSecretNumber();
         var userGuess = _userInput.ObtainValidatedNumber();
         GuessCounter++;
+        
         while (CheckGuess(userGuess, secretNumber) != GuessResult.Correct)
         {
             var userNextGuess = _userInput.ObtainValidatedNumber();
-            if (!IsSameGuess(userGuess, userNextGuess))
-            {
-                GuessCounter++;
-                userGuess = userNextGuess;
-            }
-            // UpdateGuessCounter(userGuess, userNextGuess);
+            UpdateGuessCounter(userGuess, userNextGuess);
+            userGuess = userNextGuess;
         }
 
         Console.WriteLine(
@@ -39,31 +35,27 @@ public class GuessingGame
         {
             return GuessResult.Correct;
         }
-        else if (inputNumber < secretNumber)
+        if (inputNumber < secretNumber)
         {
             Console.WriteLine("Incorrect - your guess is too low!");
             return GuessResult.UnderGuess;
         }
-        else
-        {
-            Console.WriteLine("Incorrect - your guess is too high!");
+        
+        Console.WriteLine("Incorrect - your guess is too high!");
             return GuessResult.OverGuess;
+    }
+
+    public void UpdateGuessCounter(int lastGuess, int currentGuess)
+    {
+        if (lastGuess != currentGuess)
+        {
+            GuessCounter++;
         }
     }
-
-    public bool IsSameGuess(int lastGuess, int currentGuess)
-    {
-        return lastGuess == currentGuess;
-    }
-
-
-    // public int UpdateGuessCounter(int lastGuess, int currentGuess)
+    
+    // public bool IsSameGuess(int lastGuess, int currentGuess)
     // {
-    //     if (lastGuess != currentGuess)
-    //     {
-    //         GuessCounter++;
-    //     }
-    //
-    //     return GuessCounter;
+    //     return lastGuess == currentGuess;
     // }
+
 }
